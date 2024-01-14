480643ae72294a79a159f84e622c18654ea3a88e
GeorgeOprea <oprea.george14@yahoo.com>
2024-01-14 20:39:02 +0200

    finished styling for Individual Item Page

---
+++ boutique/src/Router.jsx
@@ -0,0 +1,30 @@
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import App from "./App";
+// import Shop from "./pages/Shop";
+// import Cart from "./pages/Cart";
+// import IndividualItem from "./pages/IndividualItemPage";
+
+// const Router = () => {
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <App />,
+//     },
+//     {
+//       path: "shop",
+//       element: <Shop />,
+//     },
+//     {
+//       path: "cart",
+//       element: <Cart />,
+//     },
+//     {
+//       path: "individual",
+//       element: <IndividualItem />,
+//     },
+//   ]);
+
+//   return <RouterProvider router={router} />;
+// };
+
+// export default Router;
--- boutique/src/pages/IndividualItemPage.jsx
+++ boutique/src/pages/IndividualItemPage.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import "../styles/IndividualItemPage.css";
+import { FaCircleMinus } from "react-icons/fa6";
+import { FaCirclePlus } from "react-icons/fa6";
 
 const IndividualItem = ({ items }) => {
   const { id } = useParams();
@@ -10,20 +12,39 @@ const IndividualItem = ({ items }) => {
     <>
       {selectedItem ? (
         <div className="individual-item-container">
-          <h2 className="individual-title">{selectedItem.title}</h2>
-          <img
-            src={selectedItem.image}
-            alt={selectedItem.title}
-            className="individual-img"
-          />
-          <p className="individual-description">
-            Description: {selectedItem.description}
-          </p>
-          <p className="individual-price">Price: ${selectedItem.price}</p>
-          <p className="individual-rating">
-            Rating: {selectedItem.rating.rate} ({selectedItem.rating.count}{" "}
-            reviews)
-          </p>
+          <div>
+            <h2 className="individual-title">{selectedItem.title}</h2>
+            <img
+              src={selectedItem.image}
+              alt={selectedItem.title}
+              className="individual-img"
+            />
+            <p className="individual-description">
+              Description: {selectedItem.description}
+            </p>
+            <p className="individual-price">Price: ${selectedItem.price}</p>
+            <p className="individual-rating">
+              Rating: {selectedItem.rating.rate} ({selectedItem.rating.count}{" "}
+              reviews)
+            </p>
+          </div>
+          <div className="add-container">
+            <div className="quantity-container">
+              <button aria-label="decrease quantity" disabled="" id="minus">
+                <FaCircleMinus className="quantity-icon" />
+              </button>
+              <input
+                aria-label="item quantity"
+                type="number"
+                value="1"
+                className="quantity-input"
+              />
+              <button aria-label="increase quantity" id="plus">
+                <FaCirclePlus className="quantity-icon" />
+              </button>
+            </div>
+            <button className="add-cart-btn">Add to cart</button>
+          </div>
         </div>
       ) : (
         <div>No item found with the given ID</div>
