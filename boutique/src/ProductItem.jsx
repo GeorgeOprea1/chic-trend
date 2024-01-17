8717465427d14fae21c893b0c541efb7ae00e60e
GeorgeOprea <oprea.george14@yahoo.com>
2024-01-17 18:53:16 +0200

    fix: Heading elements are not in a sequentially-descending order

--- boutique/src/ProductItem.jsx
+++ boutique/src/ProductItem.jsx
@@ -6,7 +6,7 @@ const ProductItem = ({ id, title, image, price, rating }) => {
     <Link to={`/individual/${id}`} className="product-item">
       <img src={image} alt={title} className="product-image" />
       <div className="product-details">
-        <h3 className="title">{title}</h3>
+        <h1 className="title">{title}</h1>
         <p className="price">Price: ${price}</p>
         <p className="rating">
           Rating: {rating.rate} ({rating.count} reviews)
